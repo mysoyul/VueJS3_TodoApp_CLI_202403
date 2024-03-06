6e91fd2a7773116009dbc87bd3e0018c921d9081
mysoyul <vega2k_@naver.com>
2024-03-06 17:19:38 +0900

    todoapp level1

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="inputBox shadow">
+        <input type="text" :value="newTodoItem" @input="handleInput" @keyup.enter="addTodo">
+        <span class="addContainer" @click="addTodo">
+            <i class="fas fa-plus addBtn"></i>
+        </span>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const newTodoItem = ref("")
+
+//input:todo 라는 이벤트 정의
+const emit = defineEmits(["input:todo"])
+
+const handleInput = (event) => {
+    const todoText = event.target.value
+    if (!todoText) return
+    // input:todo 라는 이벤트 발생
+    emit("input:todo", todoText)
+    newTodoItem.value = todoText
+}
+
+const addTodo = () => {
+    const todoItem = newTodoItem.value
+    const todoItemObj = {completed: false, item:todoItem}
+    localStorage.setItem(todoItem, JSON.stringify(todoItemObj))
+    clearInput()
+}
+
+const clearInput = () => {
+    newTodoItem.value = ""
+}
+
+</script>
+
+<style scoped>
+i,span {
+    cursor: pointer;
+}
+input:focus {
+    outline: none;
+}
+
+.inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+
+.inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+    width: 80%;
+}
+
+.addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+
+.addBtn {
+    color: white;
+    vertical-align: middle;
+}
+</style>
